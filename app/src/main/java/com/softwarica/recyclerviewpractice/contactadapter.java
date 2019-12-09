84726fdd187a7279a4badee6d151635ca77adde3
hipak <hpk393@gmail.com>
2019-12-10 05:38:01 +0545

    RecyclerViewPractice

---
+++ app/src/main/java/com/softwarica/recyclerviewpractice/contactadapter.java
@@ -0,0 +1,63 @@
+package com.softwarica.recyclerviewpractice;
+
+import android.content.Context;
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class contactAdapter extends RecyclerView.Adapter<contactAdapter.ContactsViewHolder>{
+
+    Context mContext;
+    List<contracts>contractsList;
+
+    public contactAdapter(Context mContext, List<contracts> contractsList) {
+        this.mContext = mContext;
+        this.contractsList = contractsList;
+
+    }
+
+    @NonNull
+    @Override
+    public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_contracts,parent,false);
+        return new ContactsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactsViewHolder holder, int position) {
+        contracts contracts=contractsList.get(position);
+        holder.imgProfile.setImageResource(contracts.getImageId());
+        holder.tvName.setText(contracts.getName());
+        holder.tvMessage.setText(contracts.getMessage());
+        holder.tvTime.setText(contracts.getTime());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return contractsList.size();
+    }
+
+    public class ContactsViewHolder extends RecyclerView.ViewHolder{
+        CircleImageView imgProfile;
+        TextView tvName,tvMessage,tvTime;
+
+        public ContactsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgProfile=itemView.findViewById(R.id.ronaldo);
+            tvName=itemView.findViewById(R.id.tvName);
+            tvMessage=itemView.findViewById(R.id.tvMessage);
+            tvTime=itemView.findViewById(R.id.tvTime);
+        }
+    }
+}
---
+++ app/src/main/java/com/softwarica/recyclerviewpractice/contracts.java
@@ -0,0 +1,49 @@
+package com.softwarica.recyclerviewpractice;
+
+public class contracts {
+    private String name;
+    private String message;
+    private String time;
+    private int imageId;
+
+    public contracts(String name, String message, String time,int imageId) {
+        this.name = name;
+        this.message = message;
+        this.time=time;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+}
