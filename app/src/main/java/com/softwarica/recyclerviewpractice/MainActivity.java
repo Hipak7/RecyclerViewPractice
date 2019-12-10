1fd1e3ff8a8ef0d46b7207aee04095a61bf9834d
hipak <hpk393@gmail.com>
2019-12-10 06:01:39 +0545

    Recycler view Time added

--- app/src/main/java/com/softwarica/recyclerviewpractice/MainActivity.java
+++ app/src/main/java/com/softwarica/recyclerviewpractice/MainActivity.java
@@ -10,15 +10,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
+    private RecycleView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         recyclerView=findViewById(R.id.recycleView);
 
         List<contracts> contractsList=new ArrayList<>();
+        contractsList.add(new contracts("iron","I am IronMan.Don't do anything I would do, and definitely don't do anything I wouldn't do...","22:00",R.drawable.iron));
+        contractsList.add(new contracts("spider","I am Spiderman.If a lion broke out of its cage at the zoo, and bit you, it would hurt, sure, and you'd be upset, of course. But would you be offended?","21:00",R.drawable.spider));
+        contractsList.add(new contracts("thor","I am Thor.Thor once again shows his diplomatic abilities in resolving a dispute at the Stark Tower party in Age of Ultron.","14:00",R.drawable.thor));
         contractsList.add(new contracts("iron","I am IronMan.Don't do anything I would do, and definitely don't do anything I wouldn't do...","22:00",R.drawable.iron));
         contractsList.add(new contracts("spider","I am Spiderman.If a lion broke out of its cage at the zoo, and bit you, it would hurt, sure, and you'd be upset, of course. But would you be offended?","21:00",R.drawable.spider));
         contractsList.add(new contracts("thor","I am Thor.Thor once again shows his diplomatic abilities in resolving a dispute at the Stark Tower party in Age of Ultron.","14:00",R.drawable.thor));
